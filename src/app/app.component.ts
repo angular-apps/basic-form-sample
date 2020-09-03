372b0f870b975c82a7717d5cacda009a4721d88c
unknown <muhammed.albarmavi@gmail.com>
2020-09-03 11:04:08 +0300

    implement the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'basicFormSample';
+  form: FormGroup;
+
+  // 2️⃣ inject the formBuilder
+  constructor(private fb: FormBuilder) {
+    // 3️⃣ setup the from
+    this.form = fb.group({
+      username: [null, Validators.required],
+      password: [null, [Validators.required, Validators.minLength(5)]],
+      email: [null, [Validators.required, Validators.email]],
+      age: [
+        null,
+        [Validators.required, Validators.min(1), Validators.min(100)],
+      ],
+
+      address: fb.array([this.addressGroup()]),
+    });
+
+    // 4️⃣ create the form elements at the template
+  }
+
+  get address(): FormArray {
+    return this.form.get("address") as FormArray;
+  }
+
+  addressGroup() {
+    return this.fb.group({
+      name: [null, Validators.required],
+      country: [null, Validators.required],
+      city: [null, Validators.required],
+      town: [null, Validators.required],
+    });
+  }
+
+  addNewAddress() {
+    this.address.push(this.addressGroup());
+  }
+
+  removeAddress(index) {
+    this.address.removeAt(index);
+  }
+
+  save() {
+    // fake save method
+    console.log(this.form.value);
+  }
 }
